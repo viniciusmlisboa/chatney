94a1847e2199891a68f55040894417c64423ff79
Vinicius Lisboa <lisboamvinicius@gmail.com>
2025-05-25 11:55:18 -0300

    adiciona password no user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,11 @@
 const db = require("../database/db");
 
-function create(username) {
+function create(username, password) {
   try {
-    const query = db.prepare("INSERT INTO users (username) VALUES (?)");
-    const result = query.run(username);
+    const query = db.prepare(
+      "INSERT INTO users (username, password) VALUES (?, ?)"
+    );
+    const result = query.run(username, password);
     return result.lastInsertRowid;
   } catch (err) {
     if (err.code == "SQLITE_CONSTRAINT_UNIQUE") {
